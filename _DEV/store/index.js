10878f3a28a43d0ff60c9d8990843cf51fc94fb9
tomBeach <tbeach2k@gmail.com>
2018-04-16 11:40:56 -0400

    removing _DEV

--- _DEV/store/index.js
+++
@@ -1,9 +0,0 @@
-// ======= src/js/store/index.js =======
-import { createStore } from "redux";
-import initDataReducer from "../reducers/index";
-import { composeWithDevTools } from "redux-devtools-extension";
-
-// const store = createStore(initDataReducer);
-const store = createStore(initDataReducer, composeWithDevTools());
-
-export default store;
